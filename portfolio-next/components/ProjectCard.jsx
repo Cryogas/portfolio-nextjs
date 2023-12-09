bc99a2da5256c46d9832a066c05dc894c96c373d
Cryogas <136941894+Cryogas@users.noreply.github.com>
2023-12-09 17:20:50 +0800

    ProjectCard Height & Width OK

--- portfolio-next/components/ProjectCard.jsx
+++ portfolio-next/components/ProjectCard.jsx
@@ -8,22 +8,22 @@ const ProjectCard = ({imgUrl, title, description, gitUrl}) => {
         <div>
             {/* <Image src={imgUrl} height={208} width={1000} className="md:h-72 hover:opacity-30 object-contain bg-no-repeat bg-center rounded-t-xl"/> */}
             <div
-            className="h-52 w-96 rounded-xl relative group center center bg-no-repeat"
+            className="rounded-xl relative group center center bg-no-repeat transition duration-1000 hover:border-2 border-sky-400 sm:h-80 md:h-52 md:w-96 lg:h-72 lg:w-auto xl:h-80 xl:w-auto 2xl:w-auto 2xl:h-64"
             style={{ background: `url(${imgUrl})`, backgroundSize: "cover" }}>
 
-                <div className="overlay items-center justify-center top-0 left-0 w-full h-full bg-[#181818] bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-80 transition-all duration-500 ">
+                <div className="overlay rounded-xl items-center justify-center top-0 left-0 w-full h-full bg-[#181818] bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-80 transition-all duration-500 ">
                     <Link
                         href={gitUrl}
-                        className="h-14 w-14 border-2 relative rounded-full border-[#ADB7BE] hover:border-white group/link"
+                        className="h-16 w-16 border-4 relative rounded-full border-[#ADB7BE] transition duration-300 hover:border-white group/link"
                     >
-                        <CodeBracketIcon className="h-10 w-10 text-[#ADB7BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  cursor-pointer group-hover/link:text-white" />
+                        <CodeBracketIcon className="h-10 w-10 text-[#ADB7BE] absolute top-1/2 left-1/2 transform transition duration-1000 -translate-x-1/2 -translate-y-1/2  cursor-pointer group-hover/link:text-white" />
                     </Link>
                 </div>
             </div>
             
             <div className="text-white">
-                <h5 className="font-semibold text-xl mb-2">{title}</h5>
-                <p className="text-">{description}</p>
+                <h5 className="font-semibold text-xl mb-2 mt-4 text-center">{title}</h5>
+                <p className="text-center mb-4">{description}</p>
             </div>
 
         </div>
--- portfolio-next/components/Projects.jsx
+++ portfolio-next/components/Projects.jsx
@@ -11,27 +11,27 @@ const projectData = [
         gitUrl: "https://github.com/Cryogas/business-card"
 
     },
-    // {
-    //     id: 2,
-    //     title: "ClubDAO",
-    //     description: "This is a project.",
-    //     image: "/projects/clubdao.png",
-    //     gitUrl: "https://github.com/0xYudhishthra/ClubDAO"
-    // },
-    // {
-    //     id: 3,
-    //     title: "FundChat",
-    //     description: "This is a project for APUBCC Sparkathon & Solana Hyperdrive.",
-    //     image: "/projects/FundChat.png",
-    //     gitUrl: "https://github.com/cheongyeechian/fundchat"
-    // },
-    // {
-    //     id: 4,
-    //     title: "Megamask",
-    //     description: "This is a project for ETH Online.",
-    //     image: "/projects/megamask.png",
-    //     gitUrl: "https://github.com/megamask"
-    // }, 
+    {
+        id: 2,
+        title: "ClubDAO",
+        description: "This is a project.",
+        image: "/projects/clubdao.png",
+        gitUrl: "https://github.com/0xYudhishthra/ClubDAO"
+    },
+    {
+        id: 3,
+        title: "FundChat",
+        description: "This is a project for APUBCC Sparkathon & Solana Hyperdrive.",
+        image: "/projects/FundChat.png",
+        gitUrl: "https://github.com/cheongyeechian/fundchat"
+    },
+    {
+        id: 4,
+        title: "Megamask",
+        description: "This is a project for ETH Online.",
+        image: "/projects/megamask.png",
+        gitUrl: "https://github.com/megamask"
+    }, 
     // {
     //     id: 5,
     //     title: "zkJom",
@@ -52,7 +52,7 @@ const Projects = () => {
     return (
         <div id="projects">
             <div className="text-4xl font-semibold mb-8 transition duration-700 hover:text-cyan-300 ">My Project(s)</div>
-            <div className="grid grid-cols-1 md:grid-cols-2 grid-rows-2 gap-x-8 gap-y-4 md:gap-12 lg:grid-cols-4">
+            <div className="grid grid-cols-1 md:grid-cols-2 grid-rows-1 gap-x-8 gap-y-6 md:gap-12 lg:grid-cols-2 lg:gap-x-7 xl:grid-cols-2 xl:gap-10 2xl:grid-cols-4 2xl:gap-6">
                 {projectData.map((project) => (
                     <ProjectCard 
                         key={project.id}
