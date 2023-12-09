4b606c82ae1c5a54dc319171208e3308ae5c6d50
Cryogas <136941894+Cryogas@users.noreply.github.com>
2023-12-09 19:28:00 +0800

    gradient background for typewritter element done

--- portfolio-next/components/Navbar.jsx
+++ portfolio-next/components/Navbar.jsx
@@ -4,10 +4,10 @@ const Navbar = () => {
   return (
     <nav className="scrolled-header sticky top-0 left-0 w-full border-b border-[#4d4e53] shadow-sm text-xl bg-black z-10 mx-auto">
       <div className="px-12 h-24 flex justify-between items-center text-white relative">
-        <a href="#" className="md:inline hidden">Yee Chian</a>
+        <a href="#" className="md:inline hidden text-gray-400 text-2xl">Web Portfolio</a>
         <a href="#" className="md:hidden">YC</a>
         <div className="flex justify-end space-x-2">
-          <a href="#aboutMe" className="p-4">About</a>
+          <a href="#aboutMe" className="p-4 transform duration-500 hover:bg-red-400">About</a>
           <a href="#projects" className="p-4">Projects</a>
           <a href="#contact" className="p-4">Contact</a>
         </div>
